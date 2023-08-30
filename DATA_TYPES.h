1f8bf29b7d791a81a7cd6acc793fdaef6008ca7c
abdullah rashed <72138000+abdullahrashed@users.noreply.github.com>
2023-08-31 01:09:39 +0300

    Update DATA_TYPES.h

--- DATA_TYPES.h
+++ DATA_TYPES.h
@@ -1,34 +1,33 @@
 /* 
  * File:   DATA_TYPES.h
- * Author: abdul
+ * Author: abdullah rahed
  *
  * Created on August 19, 2023, 3:50 PM
  */
 
 #ifndef DATA_TYPES_H
 #define	DATA_TYPES_H
-
+/* ----------------- Includes -----------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/* ----------------- Data Type Declarations -----------------*/
 typedef unsigned char uint8;
 typedef signed char   sint8;
 typedef unsigned short uint16;
 typedef signed short sint16; 
 typedef unsigned long uint32;
 typedef signed long sint32;
-
+typedef unsigned char standard_return ;
+/* ----------------- Macro Declarations -----------------*/
 #define HIGH 1
 #define LOW 0
-
 #define ON  1
 #define OFF 0
-
 #define ok   1
 #define n_ok 0
 
-typedef unsigned char standard_return ;
+
 
 
 
